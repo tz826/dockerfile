12b6c98d7dda161b09a9c5cced8456bf270184f6
noise131 <ayur1031@163.com>
2022-08-17 13:24:09 +0800

    Upload ldap-account-manager

---
+++ ldap-account-manager/docker-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -eux
+# LDAP_URL='ldap://10.0.0.11'
+# INITIAL_DN='dc=noise131,dc=com'
+# ADMIN_USER='admin'
+
+sed -r \
+    -e "s/ldap://localhost:389$/${LDAP_URL}/g" \
+    -e "s/,dc=my-domain,dc=com$/,${INITIAL_DN}/g" \
+    -e "s/dc=yourdomain,dc=org$/${INITIAL_DN}/g" \
+    -e "s/cn=Manager/cn=${ADMIN_USER}/g" \
+    -i.bak /www/lam/config/lam.conf
+
+php-fpm -D
+
+exec "$@"
