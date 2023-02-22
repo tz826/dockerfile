b73ab86bec2b4132377012ff4373e5866861deaa
noise131 <ayur1031@163.com>
2023-02-22 21:34:23 +0800

    Create etcd

---
+++ etcd/3.4.23-debian/docker-entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+test -f /etc/etcd/etcd.conf || ( echo "etcd config file not found !" && echo 1 )
+
+if [ "${1}" != 'etcd' ]; then
+    exec "$@"
+fi
+
+if [ -z "${ETCD_DATA_DIR}" ];then
+    DATA_DIR=$( sed -nr "/^ETCD_DATA_DIR/ s/^.*\"(.*)\".*$/\1/g p" /etc/etcd/etcd.conf )
+    mkdir -p "${DATA_DIR%/*}"
+    chown -R etcd:etcd "${DATA_DIR%/*}"
+fi
+
+if [ "$(id -u)" -eq 0 ]; then
+    exec gosu etcd "$0" "$@"
+fi
+
+ETCD_CONFIG=$( grep -vE "^\s*#|^\s*$" /etc/etcd/etcd.conf |awk 'BEGIN{ i=0 } { array[i]=$0; i++ } END{ for (y=0;y<=i;y++){ printf "%s ",array[y]; } }' )
+CMD="${ETCD_CONFIG} $*"
+bash -c "$CMD"
